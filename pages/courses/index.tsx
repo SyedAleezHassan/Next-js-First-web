e4fb0ebcd0856477ee01253eb594f0969828e179
SyedAleezHassan <syedaleezhassan@gmail.com>
2024-10-30 04:09:32 +0500

    minor change

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 function Home() {
   return (
     <div>
-     <h1>course1</h1>
+     <link href='/course1' >course1</link>
      <h1>course2</h1>
      <h1>course3</h1>
      <h1>course4</h1>
+     <h1>course5</h1>
     </div>
   )
 }
